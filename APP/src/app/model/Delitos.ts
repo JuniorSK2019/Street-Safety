b23b986014f20676b587fe74326cdd310ea63841
JuniorSK2019 <85843504+JuniorSK2019@users.noreply.github.com>
2023-05-01 04:07:28 -0500

    Delete APP directory

--- APP/src/app/model/Delitos.ts
+++
@@ -1,8 +0,0 @@
-export class Delitos {
-    id:number=0;
-    Hora:string="";
-    Fecha:Date = new Date(Date.now());
-    Distrito:string="";
-    Distrito_idZona:number=0;
-    IdTipoDelitos:number=0;
-    }
